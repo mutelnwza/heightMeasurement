1f4e2a747e75fce232e03b813709666ba78e0a64
mutelnwza <mutemallow@gmail.com>
2024-05-15 14:30:19 +0700

    done

--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ import time
 img = cv2.imread("D:\\heightMeasurement\\test\\test_img\\275968.jpg")
 img = cv2.resize(img,(800,800))
 pos,heightinpixel,refinpixel = [],[],[]
+heightsum = 0
   
 def draw_circle(event, x, y, flags, param): 
       
@@ -31,12 +32,17 @@ while True:
             # heightinpixel.append(abs(pos[i][1]-pos[i+1][1]))
             img=cv2.line(img, pos[i],pos[i+1],(0,0,255),1)
             print(i)
+        for i in range(len(heightinpixel)):
+            heightsum+=heightinpixel[i]
+
 
     cv2.destroyWindow("Original Picture")
 
     cv2.imshow("heightline", img)
     cv2.setMouseCallback("heightline",draw_circle)
+
     pos=[]
+
     if cv2.waitKey(0) == ord('a'):
         for i in range(len(pos)-1):
             refinpixel.append(abs(pos[i+1][1]-pos[i][1]))
@@ -48,11 +54,11 @@ while True:
     height=float(input("input length of the reference object"))
     if cv2.waitKey(0) == ord('q'):
         print("calculating")
-        print(heightinpixel)
+        print(heightsum)
         
         pixel_per_cm = height/refinpixel[0]
         print("pixel per cm =",pixel_per_cm)
-        cm = heightinpixel[0]*pixel_per_cm
+        cm = heightsum*pixel_per_cm
         cv2.putText(img, str(cm), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
         cv2.destroyWindow("reference object")
         cv2.imshow("final height",img)
