b34975af3daa21dc9384e2642cf651457f9da67a
mutelnwza <mutemallow@gmail.com>
2024-05-16 18:50:11 +0700

    done

--- stlit.py
+++ stlit.py
@@ -2,29 +2,32 @@ import cv2
 import math
 import streamlit as st
 from PIL import Image
+from PIL import ImageDraw
 import numpy as np
 from streamlit_image_coordinates import streamlit_image_coordinates
+from streamlit_js_eval import streamlit_js_eval
 
-
-def draw_circle(x, y, img): 
-    print("position added")
-    cv2.circle(st.session_state['img'], (x, y), 3, (0,0,255), -1)   
-    st.session_state['pos'].append([x,y])
-    print(st.session_state['pos'])
-
-
-def refobj(x,y,img):
-    cv2.circle(st.session_state['img'],(x,y),3,(0,0,255),-1)
-    st.session_state['refpos'].append([x,y])
+def reset():
+    streamlit_js_eval(js_expressions="parent.window.location.reload()")
 
 
 def getdist(pos1,pos2):
     return math.dist(pos1,pos2)
 
 
-def setstate(i):
-    st.session_state.stage=i
+def stage2():
+    st.session_state.stage =2
+    st.session_state['img'] = imgraw
+    for i in range(len(st.session_state['pos'])-1):
+        st.session_state['heightinpixel'].append(getdist(st.session_state['pos'][i],st.session_state['pos'][i+1]))
 
+
+    for i in range(len(st.session_state['heightinpixel'])):
+        st.session_state['heightsum'] += st.session_state['heightinpixel'][i]
+
+def stage3():
+    st.session_state.stage =3
+    st.session_state['img'] = img
 
 if "stage" not in st.session_state:
     st.session_state.stage=0
@@ -35,6 +38,12 @@ if 'pos' not in st.session_state:
 if 'refpos' not in st.session_state:
     st.session_state['refpos'] = []
 
+if 'refinpixel' not in st.session_state:
+    st.session_state['refinpixel'] = []
+
+if 'refsum' not in st.session_state:
+    st.session_state['refsum'] = 0
+
 if 'img' not in st.session_state:
     st.session_state['img'] = None
 
@@ -44,82 +53,91 @@ if 'heightinpixel' not in st.session_state:
 if 'heightsum' not in st.session_state:
     st.session_state['heightsum'] = 0
 
+
 st.title("Height Estimating")
 
 if st.session_state.stage==0:
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         imgraw = Image.open(uploaded_file)
-        imgraw = imgraw.resize((500,500))
+        imgraw = imgraw.resize((800,800))
 
         st.session_state['img'] = np.array(imgraw)
 
         with imgraw:
+            st.text("click to mark positions of your bodyparts, position recommended:\n-top of your head\n-neck\n-hip(only one side)\n-knee (only one side)\n-ankle(only one side)\n-heel(only one side)")
+            draw = ImageDraw.Draw(imgraw)
+
+            positionmarked= len(st.session_state["pos"])
+
+            # count position marked then draw circle
+            for i in range(positionmarked):
+                circle=[st.session_state["pos"][i][0]-3,st.session_state["pos"][i][1]-3,st.session_state["pos"][i][0]+3,st.session_state["pos"][i][1]+3]
+                draw.ellipse(circle,fill="red")
+            
+            # draw line
+            if positionmarked > 1:
+                for i in range(positionmarked-1):
+                    draw.line([st.session_state['pos'][i][0],st.session_state['pos'][i][1],st.session_state['pos'][i+1][0],st.session_state['pos'][i+1][1]],fill="red",width=0)
+
             value=streamlit_image_coordinates(imgraw, key="pil")
 
-            if value is not None:
+            if value is not None and st.session_state.stage==0:
                 point = value["x"],value["y"]
-                draw_circle(point[0],point[1],st.session_state['img'])
-        
-        st.button("continue",on_click=setstate,args=[2])
+                if list(point) not in st.session_state["pos"] and list(point) not in st.session_state["refpos"]:
+                    st.session_state['pos'].append([point[0],point[1]])
+                    st.experimental_rerun()
 
-        st.image(st.session_state['img'])
+            # if position clicked is not on the list, append it then reload the website to show the lastest update
+        
+        st.button("continue",on_click=stage2)
+        st.button("reset", on_click=reset) # F5
+
 
 if st.session_state.stage ==2:
-    for i in range(len(st.session_state['pos'])-1):
-        img=cv2.circle(st.session_state['img'],(st.session_state['pos'][i]),3, (0,0,255), -1)
-        img=cv2.line(st.session_state['img'],st.session_state['pos'][i],st.session_state['pos'][i+1],(0,0,255),1)
-        st.session_state['heightinpixel'].append(getdist(st.session_state['pos'][i],st.session_state['pos'][i+1]))
 
-    for i in range(len(st.session_state['heightinpixel'])):
-        st.session_state['heightsum'] += st.session_state['heightinpixel'][i]
+    st.text(st.session_state['heightsum'])
+    
+    img = st.session_state['img']
+    st.text(st.session_state['heightinpixel'])
+    with img:
+        draw = ImageDraw.Draw(img)
+        positionmarked= len(st.session_state["refpos"])
 
-    img2 = Image.fromarray(st.session_state['img'])
+        # count position marked then draw circle
+        for i in range(positionmarked):
+            circle=[st.session_state["refpos"][i][0]-3,st.session_state["refpos"][i][1]-3,st.session_state["refpos"][i][0]+3,st.session_state["refpos"][i][1]+3]
+            draw.ellipse(circle,fill=(0,0,255))
 
-    with img2:
-        value=streamlit_image_coordinates(img2,key="pil")
+        # draw line
+        if positionmarked > 1:
+            for i in range(positionmarked-1):
+                draw.line([st.session_state['refpos'][i][0],st.session_state['refpos'][i][1],st.session_state['refpos'][i+1][0],st.session_state['refpos'][i+1][1]],fill=(0,0,255),width=0)
 
-        if value is not None:
-            point=value["x"],value["y"]
-            refobj(point[0],point[1],img2)
+        click=streamlit_image_coordinates(img, key="x")
 
-
-
-    # if cv2.waitKey(0) == ord('a'):
-    #     for i in range(len(pos)-1):
-    #         heightinpixel.append(getdist(pos[i],pos[i+1]))
-    #         # heightinpixel.append(abs(pos[i][1]-pos[i+1][1]))
-    #         img=cv2.line(img, pos[i],pos[i+1],(0,0,255),1)
-    #         print(i)
-    #     for i in range(len(heightinpixel)):
-    #         heightsum+=heightinpixel[i]
+        if click is not None:
+            point = click["x"],click["y"]
+            if list(point) not in st.session_state["refpos"] and list(point) not in st.session_state["pos"]:
+                st.session_state['refpos'].append([point[0],point[1]])
+                st.experimental_rerun()
+    
+    st.session_state['refincm'] = st.number_input("input height of the reference object (cm)")
+    st.button("start calculation", on_click=stage3)
 
 
-    # # cv2.destroyWindow("Original Picture")
+if st.session_state.stage ==3:
+    for i in range(len(st.session_state['refpos'])-1):
+        st.session_state['refinpixel'].append(getdist(st.session_state['refpos'][i],st.session_state['refpos'][i+1]))
 
-    # cv2.imshow("heightline", img)
-    # cv2.setMouseCallback("heightline",draw_circle)
+    for i in range(len(st.session_state['refinpixel'])):
+        st.session_state['refsum'] += st.session_state['refinpixel'][i]
 
-    # if cv2.waitKey(0) == ord('a'):
-    #     for i in range(len(pos)-1):
-    #         refinpixel.append(abs(pos[i+1][1]-pos[i][1]))
-    #         img=cv2.line(img,pos[i],pos[i+1],(0,0,255),1)
+    refcm = float(st.session_state['refincm'])
+    pixel_per_cm = refcm/st.session_state['refsum']
+    heightestimated = st.session_state['heightsum']*pixel_per_cm
 
-    # cv2.destroyWindow("heightline")
+    img = st.session_state['img']
 
-    # cv2.imshow("reference object",img)
-    # height=float(input("input length of the reference object"))
-    # if cv2.waitKey(0) == ord('q'):
-    #     print("calculating")
-    #     print(heightsum)
-        
-    #     pixel_per_cm = height/refinpixel[0]
-    #     print("pixel per cm =",pixel_per_cm)
-    #     cm = heightsum*pixel_per_cm
-    #     cv2.putText(img, str(cm), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
-    #     cv2.destroyWindow("reference object")
-    #     cv2.imshow("final height",img)
-        
-        
-
-    # cv2.destroyAllWindows() 
+    st.image(img)
+    st.text(heightestimated)
